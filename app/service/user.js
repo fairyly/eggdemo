126fa4c5f44df31a14e257822e8a14877c5ea950
fairyly <fairyilys@gmail.com>
2019-08-05 22:57:28 +0800

    fix: 修改打印

--- app/service/user.js
+++ app/service/user.js
@@ -293,7 +293,7 @@ class UserService extends Service {
       let hasSign = false;
       let dateVal = `${y}-${m}-${d}`;
       const hasSignData = await ctx.model.Sign.find({ openid: requestParam.openid,createDate:{ $in: dateVal} });
-      console.log(hasSignData,dateVal,hasSignData.createDate.includes(dateVal))
+      console.log(hasSignData)
       ctx.coreLogger.info('数据：', hasSignData);
       if (!!hasSignData && hasSignData.createDate.includes(dateVal)) {
         hasSign = true;
